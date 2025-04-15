a48ce8e1c345204310c9ddb54c206e162841de92
Karishma2422 <karishmapatil74@gmail.com>
2025-04-15 15:44:08 +0530

    updates

--- index.js
+++ index.js
@@ -1,25 +1,68 @@
 import express from "express";
+import { Login, Register } from "./controllers/auth.controllers.js";
+import { Product } from "./controllers/product.controllers.js";
 import AllRoutes from "./routes/index.js";
 import morgan from "morgan";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-import cors from "cors";
+import dotenv from "dotenv"
+
+console.log("Hello we are from Index");
+
+// here we are going to create express server
+
+const books = [{ id: 1, name: "Rich dad ", author: "Marry Dsouza" }];
 
 const app = express();
 
-app.use(express.json());
-app.use(morgan("combined"));
+app.use(express.json())
+app.use(morgan("combined"))
 dotenv.config();
-app.use(cors());
+
+app.use("/api/v1", AllRoutes)
+
+mongoose.connect(process.env.MONGODBURL).then(() => { console.log("Mongo DB Connected") })
 
 app.get("/", (req, res) => {
-  return res.send("Welcome to backend server.");
+  res.send("Welcome to Express Server 0_0 ");
+});
+app.get("/books", (req, res) => {
+  console.log("Inside Books!")
+  res.send(`${books.map((book) => (
+    `${book.id}. ${book.name} by ${book.author}`
+  ))}`)
 });
 
-app.use("/api/v1", AllRoutes);
+app.get("/products", (req, res) => {
+  console.log("Inside Products!")
 
-mongoose.connect(process.env.MONGODBURL).then(() => {
-  console.log("MongoDb conected.");
+  res.send("My Products...")
+})
+
+app.get("/greet", (req, res) => {
+  console.log("Inside Greet!")
+
+  res.send("Welcome!")
+})
+
+app.post("/register", Register);
+app.post("/products", Product);
+app.post("/login", Login);
+
+app.get("/hello", (req, res) => {
+  res.send("Welcome to Aincrad")
+})
+
+app.get("/sao", (req, res) => {
+  res.send("Welcome to SAO")
+})
+
+app.use((req, res) => {
+  res.status(404).send("OOPS Looks like you are lost >_<");
 });
 
-app.listen(8000, () => console.log("Server is running on port 8000"));
+
+const backendPort = 8000;
+
+app.listen(backendPort, () => {
+  console.log(`Server is running on port ${backendPort}`);
+});
